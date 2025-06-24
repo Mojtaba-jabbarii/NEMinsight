eca867673db04e4f8f1f222734a456a5256ee35a
Mojtaba-jabbarii <mojtaba_jabbarii@yahoo.com>
2025-06-24 22:15:11 +1000

    remove testimonial

--- js/main.js
+++ js/main.js
@@ -74,30 +74,6 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
     // Vendor carousel
     $('.vendor-carousel').owlCarousel({
         loop: true,
